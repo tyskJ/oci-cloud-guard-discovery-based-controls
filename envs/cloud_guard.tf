76e3d1f28cc24ff49190d6dd49888f62a9a453a6
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-08 18:55:09 +0900

    :memo: ...

---
+++ envs/cloud_guard.tf
@@ -0,0 +1,9 @@
+/************************************************************
+Configuration
+************************************************************/
+resource "oci_cloud_guard_cloud_guard_configuration" "this" {
+  compartment_id        = var.tenancy_ocid
+  reporting_region      = local.region_map["NRT"]
+  status                = "ENABLED" # ENABLED or DISABLED
+  self_manage_resources = false
+}
