1b3328b3da57ca60b61263de438d567eed777248
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-08 17:22:16 +0900

    :memo: Region map 追加

---
+++ envs/data.tf
@@ -0,0 +1,9 @@
+data "oci_identity_regions" "regions" {
+}
+
+# output "tokyo_region" {
+#   value = lookup({
+#     for r in data.oci_identity_regions.regions.regions :
+#     r.key => r.name
+#   }, "NRT")
+# }
