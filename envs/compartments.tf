812d79b677d6b8d9134a4f2bd41753b029407d2f
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-08 12:17:20 +0900

    :recycle: 作業コンパートメント作成

---
+++ envs/compartments.tf
@@ -0,0 +1,17 @@
+/************************************************************
+Compartment - workload
+************************************************************/
+resource "oci_identity_compartment" "parent" {
+  compartment_id = var.tenancy_ocid
+  name           = "Parent"
+  description    = "Parent For oci-cloud-guard-discovery-based-controls"
+  enable_delete  = true
+}
+
+resource "oci_identity_compartment" "child" {
+  compartment_id = oci_identity_compartment.parent.id
+  name           = "Child"
+  description    = "Child For oci-cloud-guard-discovery-based-controls"
+  enable_delete  = true
+  defined_tags   = local.common_defined_tags
+}
