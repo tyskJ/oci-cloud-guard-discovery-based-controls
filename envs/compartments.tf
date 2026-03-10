6079184819bd2e5c0ac0c5cf19b43ee792d5f212
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-10 23:17:28 +0900

    :memo: Default Tags削除

--- envs/compartments.tf
+++ envs/compartments.tf
@@ -1,17 +1,9 @@
 /************************************************************
 Compartment - workload
 ************************************************************/
-resource "oci_identity_compartment" "parent" {
+resource "oci_identity_compartment" "workload" {
   compartment_id = var.tenancy_ocid
-  name           = "Parent"
-  description    = "Parent For oci-cloud-guard-discovery-based-controls"
+  name           = "oci-cloud-guard-discovery-based-controls"
+  description    = "Workload For oci-cloud-guard-discovery-based-controls"
   enable_delete  = true
-}
-
-resource "oci_identity_compartment" "child" {
-  compartment_id = oci_identity_compartment.parent.id
-  name           = "Child"
-  description    = "Child For oci-cloud-guard-discovery-based-controls"
-  enable_delete  = true
-  defined_tags   = local.common_defined_tags
 }
