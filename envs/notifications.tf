38e193c9d7feefe506ef7bc7cd084a10c35431a9
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-12 21:16:57 +0900

    :memo: 設定値にはないため削除

--- envs/notifications.tf
+++ envs/notifications.tf
@@ -14,12 +14,4 @@ resource "oci_ons_subscription" "this" {
   topic_id       = oci_ons_notification_topic.this.topic_id
   protocol       = "EMAIL"
   endpoint       = var.subscription_email
-  delivery_policy = jsonencode({
-    backoffRetryPolicy = {
-      initialDelayInFailureRetry = 60000
-      maxRetryDuration           = 7200000
-      policyType                 = "EXPONENTIAL"
-    }
-    maxReceiveRatePerSecond = 0
-  })
 }
