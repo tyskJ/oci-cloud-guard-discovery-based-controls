5e1ea23976bdd939c50954d46ecd250513f4cf0e
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-13 11:47:40 +0900

    :memo: root compartment用イベントルール追加

--- envs/events.tf
+++ envs/events.tf
@@ -1,9 +1,26 @@
 /************************************************************
 Events
 ************************************************************/
-resource "oci_events_rule" "this" {
+resource "oci_events_rule" "root" {
+  compartment_id = var.tenancy_ocid
+  display_name   = "cloud-guard-event-root"
+  is_enabled     = true
+  condition = jsonencode({
+    eventType = ["com.oraclecloud.cloudguard.problemdetected"]
+    data      = {}
+  })
+  actions {
+    actions {
+      is_enabled  = true
+      action_type = "ONS"
+      topic_id    = oci_ons_notification_topic.this.topic_id
+    }
+  }
+}
+
+resource "oci_events_rule" "child" {
   compartment_id = oci_identity_compartment.workload.id
-  display_name   = "cloud-guard-event"
+  display_name   = "cloud-guard-event-child"
   is_enabled     = true
   condition = jsonencode({
     eventType = ["com.oraclecloud.cloudguard.problemdetected"]
